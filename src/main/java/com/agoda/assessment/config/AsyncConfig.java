32f218741cc8f23e202ea3afe569b9bc6bf3f1e1
rfchief <rfchief@gmail.com>
2017-11-24 00:39:29 +0900

    * Add EnableAsync annotation in AsyncConfig class

--- src/main/java/com/agoda/assessment/config/AsyncConfig.java
+++ src/main/java/com/agoda/assessment/config/AsyncConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
 
     private final int corePoolSize;
